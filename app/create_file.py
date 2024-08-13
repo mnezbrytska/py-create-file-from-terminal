66e611309e4e27c8cde5d728161f0748e81a2110
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-13 15:38:09 +0300

    fixed

--- app/create_file.py
+++ app/create_file.py
@@ -3,7 +3,25 @@ import os
 from datetime import datetime
 
 
-def create_dir_and_file() -> None:
+def create_file(file_path: str) -> None:
+    with open(file_path, "a") as file:
+        timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        file.write(f"\n{timestamp}\n")
+        line_number = 1
+
+        while True:
+            content = input(f"Enter line {line_number}: ")
+            if content.lower() == "stop":
+                break
+            file.write(f"{line_number} {content}\n")
+            line_number += 1
+
+
+def create_dir(dir_path: str) -> None:
+    os.makedirs(dir_path)
+
+
+def read_command() -> None:
     command_from_terminal = sys.argv
     dir_path = ""
 
@@ -15,25 +33,14 @@ def create_dir_and_file() -> None:
                 [dir_name:sys.argv.index("-f")
                     if "-f" in sys.argv else len(sys.argv)])
         )
-        os.makedirs(dir_path)
+        create_dir(dir_path)
 
     if "-f" in command_from_terminal:
         file_index = sys.argv.index("-f") + 1
         filename = sys.argv[file_index]
         file_path = os.path.join(dir_path, filename) if dir_path else filename
-
-        with open(file_path, "a") as file:
-            timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            file.write(f"\n{timestamp}\n")
-            line_number = 1
-
-            while True:
-                content = input(f"Enter line {line_number}: ")
-                if content.lower() == "stop":
-                    break
-                file.write(f"{line_number} {content}\n")
-                line_number += 1
+        create_file(file_path)
 
 
 if __name__ == "__main__":
-    create_dir_and_file()
+    read_command()
